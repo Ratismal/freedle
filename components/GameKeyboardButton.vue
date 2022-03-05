5bfdd05e486aa437eade151673cc2ab224144187
stupid cat <cat@blargbot.xyz>
2022-03-04 21:50:02 -0700

    fix key text colors

--- components/GameKeyboardButton.vue
+++ components/GameKeyboardButton.vue
@@ -2,7 +2,7 @@
   <button class="keyboard-button" :class="className" @click.prevent="click">
     <template v-if="button === 'Backspace'">
       <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-        <path fill="white" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z" />
+        <path fill="var(--color-key-text)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z" />
       </svg>
     </template>
     <template v-else>
@@ -88,20 +88,24 @@ export default {
   justify-content: center;
   height: 58px;
   flex: 1;
-  color: white;
+  color: var(--color-key-text);
   border-radius: 5px;
   margin: 0 6px 0 0;
   text-transform: uppercase;
   cursor: pointer;
+  font-weight: bold;
 
   &.present {
     background: var(--color-present);
+    color: white;
   }
   &.correct {
     background: var(--color-correct);
+    color: white;
   }
   &.absent {
     background: var(--color-absent);
+    color: white;
   }
 }
 
