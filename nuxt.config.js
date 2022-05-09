e004b1330a1e7b4a03d23f6f50b294548d1fa01a
stupid cat <cat@blargbot.xyz>
2022-05-09 10:09:19 -0600

    change to percent

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   server: {
-    port: 3006
+    port: 3006,
+    host: '0.0.0.0'
   },
 
   // Target: https://go.nuxtjs.dev/config-target
